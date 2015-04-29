d5c28c106df020c6dd78591676acddfce45a9d70
Flavio Calvo <flavio.calvo@irsol.ch>
2015-04-29 10:31:54 +0200

    Added pybold script to make simple statistics from out files

---
+++ pytools/coboldStats.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import re
+import numpy as np
+
+def check_ext(filename) :
+	if not filename.endswith('.out') :
+		raise argparse.ArgumentTypeError("%s has not a recognized extension." % filename)
+        return filename
+
+description = 'Parse CO5BOLD out files to make statistics.'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('input', help='CO5BOLD out file(s).', type=check_ext, nargs='+')
+args = parser.parse_args()
+
+phystime = []
+cputime = []
+mhdtime = []
+rttime = []
+visctime = []
+uiotime = []
+for filename in args.input :
+	time0 = -1
+	time1 = -1
+	with open(filename) as f :
+		lines = f.readlines()
+	for l in lines :
+		m = re.match(r'.*itime=    (.*)  time= (.*)  t_job',l)
+		if m != None:
+			if time0 < 0: time0 = float(m.group(2))
+			time1 = float(m.group(2))
+		m = re.match(r'RHD code',l)
+		if m != None:
+			cputime += [float(l.split()[3])]
+		m = re.match(r'Magneto-Hydrodynamics routines',l)
+		if m != None:
+			mhdtime += [float(l.split()[3])]
+		m = re.match(r'Radiation transport routines',l)
+		if m != None:
+			rttime += [float(l.split()[4])]
+		m = re.match(r'Viscosity routines 3D',l)
+		if m != None:
+			visctime += [float(l.split()[4])]
+		m = re.match(r'uio output routines',l)
+		if m != None:
+			uiotime += [float(l.split()[4])]
+	phystime += [time1-time0]
+
+phystime = np.array(phystime)
+cputime = np.array(cputime)
+mhdtime = np.array(mhdtime)
+rttime = np.array(rttime)
+rate = phystime/cputime*3600.*24.
+mhdratio = mhdtime/cputime
+rtratio = rttime/cputime
+viscratio = visctime/cputime
+uioratio = uiotime/cputime
+
+tabs = 20
+print('Simulation rate:\t{:0.0f} +/- {:0.0f} seconds/day'.format(np.mean(rate), np.std(rate)).expandtabs(tabs))
+print('MHD time:\t{:0.0f}% +/- {:0.0f}%'.format(100.*np.mean(mhdratio), 100.*np.std(mhdratio)).expandtabs(tabs))
+print('RT time:\t{:0.0f}% +/- {:0.0f}%'.format(100.*np.mean(rtratio), 100.*np.std(rtratio)).expandtabs(tabs))
+print('Viscosity time:\t{:0.0f}% +/- {:0.0f}%'.format(100.*np.mean(viscratio), 100.*np.std(viscratio)).expandtabs(tabs))
+print('I/O time:\t{:0.0f}% +/- {:0.0f}%'.format(100.*np.mean(uioratio), 100.*np.std(uioratio)).expandtabs(tabs))
