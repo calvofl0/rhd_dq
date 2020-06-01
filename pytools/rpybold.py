db87529d245a914d6149483328707cbde87a11a3
Flavio Calvo <flavio.calvo@irsol.ch>
2020-06-01 12:24:56 +0200

    Adapted rpybold to run also with Python 3

--- pytools/rpybold.py
+++ pytools/rpybold.py
@@ -2,11 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from threading import Thread
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-import xmlrpclib
-import cPickle as pickle
+try:
+    from xmlrpc.server import SimpleXMLRPCServer
+    import xmlrpc.client as xmlrpclib
+except:
+    from SimpleXMLRPCServer import SimpleXMLRPCServer
+    import xmlrpclib
+try:
+    import cPickle as pickle
+except:
+    import pickle
+from base64 import b64encode, b64decode
 import zlib
 from socket import gethostname
+
+#PICKLE_PROTOCOL = pickle.HIGHEST_PROTOCOL
+PICKLE_PROTOCOL = 2
 
 class Server(Thread):
 	def __init__(self, host=gethostname(), port=3000):
@@ -24,12 +35,12 @@ class Server(Thread):
 		def get_var(var_str):
 			d = self._dict
 			if var_str == '':
-				cstr = zlib.compress(pickle.dumps(d, pickle.HIGHEST_PROTOCOL),9)
+				cstr = zlib.compress(pickle.dumps(d, PICKLE_PROTOCOL),9)
 			else:
-				exec 'q = d.'+var_str in locals()
+				exec('q = d.'+var_str, locals())
 				var=q
 				if hasattr(var, 'value'): var = var.value
-				cstr = zlib.compress(pickle.dumps(var, pickle.HIGHEST_PROTOCOL),9)
+				cstr = zlib.compress(pickle.dumps(var, PICKLE_PROTOCOL),9)
 			return xmlrpclib.Binary(cstr)
 		self._server.register_function(get_var,'get')
 		self._running = True
